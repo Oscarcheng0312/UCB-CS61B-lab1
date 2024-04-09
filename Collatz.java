0b324317964b5a451eef99c0d660e95a168e07ca
Oscarcheng0312 <157415876+Oscarcheng0312@users.noreply.github.com>
2024-04-08 23:49:18 -0700

    Update Collatz.java

--- Collatz.java
+++ Collatz.java
@@ -2,26 +2,18 @@
  *  Oscar Cheng YOUR NAME HERE
  */
 public class Collatz {
-
-    /** the function is called Collatz sequence if n is odd, return 3n+1 else return n/2 */
+    /** Buggy implementation of nextNumber! */
     public static int nextNumber(int n) {
-        if (n % 2 == 0) {
-            return n/2;
-        } else {
-            return 3 * n + 1;
-        }
+        return n % 2 == 0 ? n/2 : 3*n + 1;
     }
 
-    /** main function for our Collatz sequence */
     public static void main(String[] args) {
         int n = 5;
-        System.out.println(n);
-            while(n > 1){
-                System.out.println(nextNumber(n));
-                n = nextNumber(n);
-
+        System.out.print(n + " ");
+        while (n != 1) {
+            n = nextNumber(n);
+            System.out.print(n + " ");
         }
+        System.out.println();
     }
-
 }
-
